972479ae4492aa6bead8a6b7246385a5f6cd92c2
liuyaxiong <779025704@qq.com>
2019-03-09 20:26:42 +0800

    项目初始化

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import './App.css';
+
+function Login() {
+  return <div>Login</div>
+}
+function Main() {
+  return <div>Main</div>
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Login}></Route>
+          <Route path="/home" component={Main}></Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
