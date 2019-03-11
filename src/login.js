a81f81c71c860458bb645b5a186f98824d2e03af
liuyaxiong <779025704@qq.com>
2019-03-11 15:59:59 +0800

    完成登录

--- src/login.js
+++ src/login.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import axios from 'axios'
 // 导入相关组件
 import { Icon, Form, Button, Divider } from 'semantic-ui-react'
+import {withRouter} from 'react-router-dom'
 import './login.css'
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+  handleUsername = (event) => {
+    this.setState({
+      username:event.target.value
+    })
+  }
+  handlePassword = (event) => {
+    this.setState({
+      password:event.target.value
+    })
+  }
+  submit = async () => {
+    // 保存token信息到sessionstorage中
+    let ret = await axios.post('http://47.96.21.88:8086/users/login', {
+      uname:this.state.username,
+      pwd:this.state.password
+    })
+    // console.log(ret)
+    // 存储token信息到sessionstorage中
+    sessionStorage.setItem('mytoken',ret.data.data.token);
+    // 跳转到主页
+    let { history } = this.props;
+    history.push('/home')
+  }
   render() {
     return (
       <div className="login-container">
@@ -18,6 +50,8 @@ class Login extends React.Component {
               size="big"
               iconPosition="left"
               name="username"
+              value={this.state.username}
+              onChange={this.handleUsername}
               placeholder="请输入用户名..."
             />
             <Form.Input
@@ -26,18 +60,22 @@ class Login extends React.Component {
               size="big"
               iconPosition="left"
               name="username"
+              value={this.state.paaword}
+              onChange={this.handlePassword}
               placeholder="请输入用户名..."
             />
-            <Button fluid color="green">登录</Button>
+            <Button onClick={this.submit} fluid color="green">
+              登录
+            </Button>
           </Form>
           <Divider horizontal>----</Divider>
         </div>
-        <div className='login-third'>
-        <Icon name="home" size="huge" color="black" />
-        <Icon name="home" size="huge" color="black" />
+        <div className="login-third">
+          <Icon name="rocketchat" size="huge" color="black" />
+          <Icon name="qq" size="huge" color="black" />
         </div>
       </div>
     )
   }
 }
-export default Login
+export default withRouter(Login);
