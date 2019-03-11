1bdc769bd4f90da20ad549598045a8ecec46d91c
liuyaxiong <779025704@qq.com>
2019-03-11 17:41:21 +0800

    主页菜单栏大概样式

--- src/module/main.js
+++ src/module/main.js
@@ -1,15 +1,41 @@
 import React from 'react'
 import './main.css'
+import { Grid, Icon } from 'semantic-ui-react'
 
 class Main extends React.Component {
   render() {
     return (
-      <div className='main-container'>
-        <div className='main-content'>
-          内容
-        </div>
-        <div className='main-menu'>
-          菜单
+      <div className="main-container">
+        <div className="main-content">内容</div>
+        <div className="main-menu">
+          <Grid columns={4} divided>
+            <Grid.Row>
+              <Grid.Column>
+                <div className="menu-icon">
+                  <Icon name="home" size="small" color="orange" />
+                </div>
+                <div className="menu-name">菜单</div>
+              </Grid.Column>
+              <Grid.Column>
+                <div className="menu-icon">
+                  <Icon name="home" size="small" color="orange" />
+                </div>
+                <div className="menu-name">菜单</div>
+              </Grid.Column>
+              <Grid.Column>
+                <div className="menu-icon">
+                  <Icon name="home" size="small" color="orange" />
+                </div>
+                <div className="menu-name">菜单</div>
+              </Grid.Column>
+              <Grid.Column>
+                <div className="menu-icon">
+                  <Icon name="home" size="small" color="orange" />
+                </div>
+                <div className="menu-name">菜单</div>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </div>
       </div>
     )
